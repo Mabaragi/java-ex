205666cfffe7cf4487472c41ace5377d35c652ae
Mabaragi <gigig9647@gmail.com>
2026-02-19 17:47:56 +0900

    commit

--- WS_03_BankTemplate/src/com/ws03/dto/AccountDto.java
+++ WS_03_BankTemplate/src/com/ws03/dto/AccountDto.java
@@ -4,7 +4,17 @@ package com.ws03.dto;
  *  고객의 계좌정보 관리하는 객체
  * */
 public class AccountDto {
+  int accountSeq;
+  int accountNumber;
+  int balance;
+  int userSeq;
   
+  public AccountDto() {}
 
-  
+  public AccountDto(int accountSeq, int accountNumber, int balance, int userSeq) {
+	this.accountSeq = accountSeq;
+	this.accountNumber = accountNumber;
+	this.balance = balance;
+	this.userSeq = userSeq;
+  }
 }
--- WS_03_BankTemplate/src/com/ws03/dto/UserDto.java
+++ WS_03_BankTemplate/src/com/ws03/dto/UserDto.java
@@ -3,7 +3,10 @@ package com.ws03.dto;
  *  고객의 정보를 관리할 객체 
  * */
 public class UserDto {
+  int userSeq;
+  String name;
+  String email;
+  String Phone;
+  boolean isSleep;
   
-   
-   
 }
